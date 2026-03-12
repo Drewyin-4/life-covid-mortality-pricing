04612c7ba6335c96d15cda4d424c0fe5306a5c6b
Drew Yin <zillyin4@gmail.com>
2026-03-12 19:49:46 -0400

    remove some duplicated steps

--- scripts/04_life_table.R
+++ scripts/04_life_table.R
@@ -79,5 +79,51 @@ glimpse(life_table)
 head(life_table)
 summary(life_table)
 
-write_csv(life_table, "data/processed/life_table_2019_female.csv")
+write_csv(life_table, "data/processed/life_table_2019_female_with_LxTxex.csv")
 
+
+build_life_table <- function(data, year_value, sex_value){
+  
+  mort_data <- data %>%
+    filter(Year == year_value, sex == sex_value) %>%
+    arrange(age)
+  
+  life_data <- mort_data %>%
+    select(age, qx) %>%
+    mutate(px = 1 - qx)
+  
+  life_data$lx <- NA
+  life_data$lx[1] <- 100000
+  
+  for(i in 2:nrow(life_data)){
+    life_data$lx[i] <- life_data$lx[i-1] * life_data$px[i-1]
+  }
+  
+  life_data <- life_data %>%
+    mutate(
+      dx = lx * qx,
+      lx_next = lead(lx),
+      Lx = if_else(is.na(lx_next), lx/2, (lx + lx_next)/2)
+    )
+  
+  life_data$Tx <- rev(cumsum(rev(life_data$Lx)))
+  
+  life_data <- life_data %>%
+    mutate(ex = Tx / lx)
+  
+  return(life_data)
+}
+
+
+life_2019_f <- build_life_table(mort_clean, 2019, "Female")
+life_2020_f <- build_life_table(mort_clean, 2020, "Female")
+
+life_2019_m <- build_life_table(mort_clean, 2019, "Male")
+life_2020_m <- build_life_table(mort_clean, 2020, "Male")
+
+
+write_csv(life_2019_f, "data/processed/life_table_2019_female.csv")
+write_csv(life_2020_f, "data/processed/life_table_2020_female.csv")
+
+write_csv(life_2019_m, "data/processed/life_table_2019_male.csv")
+write_csv(life_2020_m, "data/processed/life_table_2020_male.csv")
